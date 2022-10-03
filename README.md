156235754b596fcf468e88ef3f494d1ec01703e4
Stoll, Jonathan <git@jonathanstoll.de>
2022-10-03 10:49:59 +0200

    Release Version 2.0.0

--- README.md
+++ README.md
@@ -1,11 +1,156 @@
-SmartyML
-========
+# SmartyML
 
-This class extends the Smarty Template Engine with multilanguage support. I "stole" the idea from André Rabold who published his code on this page:
+SmartyML adds multilingual support for the popular PHP-written template engine Smarty.
+The current version, **SmartyML 2**, supports Smarty 3 or newer, including Smarty 4.
+For Smarty 2.x, please check out older versions of SmartyML, like SmartyML 1.x.
+
+## About
+SmartyML extends Smarty by multilingual support.
+Placeholders in templates are replaced by translations during template compilation.
+Once a template is compiled, there is no further latency impact in comparison without using SmartyML.
+
+Because variables in templates are handled after compilation, SmartyML also offers the replacement of placeholders for assigned strings.
+This enables using placeholders in variables, e.g. for alerts or other dynamic values.
+
+## Requirements (SmartyML 2.x)
+* Smarty 3 or newer
+  * https://github.com/smarty-php/smarty
+  * SmartyML officially supports Smarty 4
+* PHP 7.4 or newer
+  * Official support for PHP 8.0 and 8.1
+* mbstring-extension for PHP
+
+## Installation
+1. Add SmartyML-class to your repository and make sure, your classloader can find the class.
+2. Create a directory, where translation files will be stored. This directory is typically named "locales" and is located at the root directory of the project.
+3. Create files for each language you want to support in the locales directory. Nonexisting files for defined languages will cause an exception.
+
+### SPFW
+If you use the SPFW framework, SmartyML is already included and ready to use.
+
+## Configuration
+
+### Language files
+For each language, a language file has to be created in the locales directory.
+The file name has to match the language name combined with the postfix ".txt", e.g. "en.txt" for English.
+
+The language file contains key-value pairs.
+The keys are placeholders for later usage in template files.
+Each line can contain one definition.
+The key and value are separated by an equals symbol, like:
+
+```
+question=Möchten Sie fortfahren
+Yes=Ja
+No=Nein
+```
+
+A few noticeable information:
+* Empty lines are ignored
+* Lines, beginning with double slash "//", are also ignored
+* The first equal symbol is interpreted as key-value separator, while the value is able to contain unlimited amounts of equal symbols. This means an equal symbol cannot be used as part of the placeholder.
+* A double number sign cannot be used as part of the placeholder, because it is used for indicating a placeholder in templates (see Usage > Templates section in this README).
+* SmartyML does not check, if all language files define the identical placeholders. Also, the order of definitions does not matter.
+
+### Setting up languages
+Because SmartyML extends Smarty, it can be used just like Smarty.
+Replace Smarty-class by SmartyML-class wherever you need a template with multilingual support.
+By default, the English language is the available and default language.
+
+To use more than one language (the reason, why you use SmartyML ;-)), you need to make them manually available.
+This and other multilingual options are set in the constructor of SmartyML:
+* Available languages,
+* default language (default: "en" for English),
+* current language (default: default language) and
+* location of the locales directory, when other than _../locales/_.
+
+Example:
+```
+$available_languages = ['de', 'en', 'es'];  // default: ['en']
+$default_language = 'de';                   // default: 'en'
+$current_language = 'es';                   // default: default language, here: 'en'
+$locales_dir = '../../lang/';               // default: '../locales/'
+
+$smarty = new SmartyML(
+    $available_languages,
+    $default_language,
+    $current_language,
+    $locales_dir
+);
+```
+
+Unfortunately these parameters always have to be set, when SmartyML is instantiated.
+If SmartyML is used at a few different places, you should consider an additional solution.
+A solution strongly depends on how your settings are defined (hopefully not global variables).
+There are a few popular techniques how to mitigate the overhead:
+* Create a class which returns a pre-configured SmartyML-object (like View-class in SPFW) or
+* extend SmartyML by an init-method, which returns a pre-configured SmartyML-object.
+
+Note:
+It is recommended to use the short forms of a language, e.g. "es" for Spanish.
+Short forms can be useful when combining different techniques for localization or internationalization.
+SmartyML does not force to use a specific language terminology.
+
+### SPFW
+The SPFW-version of SmartyML has only one difference to the stand-alone-version:
+There is an init-method for initializing SmartyML by SmartyMlConfig.
+The SmartyMlConfig-class is provided by SPFW and can be used for defining Smarty-configuration.
+This makes it easier to create instances of SmartyML, because constructor variables are already filled.
+
+## Usage
+To use the power of translation, you just need to use the defined placeholders in your existing templates.
+
+### Templates
+Each defined placeholder in language files can be used in the templates when wrapped by two number signs: `##placeholder##`.
+
+Example:
+```
+<div>
+    <p>##question##?</p>
+    <div>
+        <a href="agree">##Yes##</a>
+        <a href="abort">##No##</a>
+    </div>
+</div>
+```
+
+This example contains the placeholders _question_, _Yes_ and _No_.
+
+If a placeholder is not defined in a translation file, it is ignored (technically replaced by empty string).
+
+#### Special variable
+To access the current language, SmartyML assigns the variable `$lang` to the template.
+This can be useful in master templates
+
+Example:
+```
+<html lang="{$lang}">
+[...]
+</html>
+```
+
+### Assigning variables
+When variables are assigned by Smarty's `assign()`-method, they are not modified.
+To make SmartyML translating placeholders in assigned variables, they need to be assigned by `assignML()`-method.
+`assignML()` has the same method signature than Smarty's `assign()` (see Smarty docs: https://smarty-php.github.io/smarty/designers/language-variables/language-assigned-variables.html).
+While `assignML()` accepts any type of variable, **only strings are taken into account for translation**.
+
+Example:
+```
+$smartyML_object->assignML(
+    'question',
+    '##question##?'
+);
+```
+
+## Contact / Support
+If you have any questions, please feel free to use the issue section on GitHub.
+I am happy to receive bug reports to make SmartyML even better.
+When you fix a bug yourself, please consider opening a pull request for SmartyML on GitHub.
+
+For the SPFW integration, please contact the SPFW community.
+
+## Credits
+SmartyML 2.0 is a full new development for PHP > 7 and Smarty > 3.x.
+The original idea is based on published code from André Rabold on the previous official Smarty discussion board:
 http://smarty.incutio.com/?page=SmartyMultiLanguageSupport
-
-I optimized the code for Smarty 2 and PHP 5. That means it is not compatible with PHP 4 or older!
-
-I am working on a version for Smarty 3. It´s not done yet.
-
-A demo and documentation will be added later.
---
+++ SmartyML.php
@@ -0,0 +1,258 @@
+<?php
+
+
+/**
+ * Smarty Multilingual 2.0
+ *
+ * @author Jonathan Stoll
+ * @version 11
+ */
+class SmartyML extends Smarty
+{
+	private const DEFAULT_LOCALES_DIR = '../locales/';
+
+
+	/** @var string[] Allowed languages */
+	private array $languages;
+
+	/** @var array<non-empty-array<string,mixed>> Key = var-name, value = array of [mixed value, bool nocache] */
+	private array $cached_variables = [];
+
+	/** @var string Default language */
+	private string $default_language;
+
+	/** @var string Current language */
+	private string $language;
+
+	/** @var string Directory where locale files are stored */
+	private string $locales_dir;
+
+	/** @var array<string,string> Key = match, value = replacement */
+	private array $translations = [];
+
+
+	/**
+	 * Creates a new smarty object and sets a language.
+	 * If argument $lang is not defined or equals null, system default language will be used.
+	 *
+	 * @param string[] $allowed_languages List of allowed languages
+	 * @param string $default_language Default language, which must be element of allowed languages
+	 * @param null|string $language Current language or null for default language
+	 *
+	 * @throws SmartyException Language is invalid or filter couldn't been set up
+	 */
+	public function __construct(array $allowed_languages, string $default_language, string $language = null, string $locales_dir = self::DEFAULT_LOCALES_DIR)
+	{
+		$valid_default_language = in_array($default_language, $allowed_languages, true);
+		if (!$valid_default_language) {
+			throw new SmartyException('Invalid default language "' . $default_language . '"');
+		}
+
+		$valid_language = $language === null || in_array($language, $allowed_languages, true);
+		if (!$valid_language) {
+			throw new SmartyException('Invalid language "' . $language . '"');
+		}
+
+		parent::__construct();
+
+		$this->languages = $allowed_languages;
+		$this->default_language = $default_language;
+		$this->language = $language ?? $default_language;
+		$this->locales_dir = $locales_dir;
+
+		// registers prefilter for language decode automatically
+		$this->registerFilter('pre', [$this, 'lang_decode']);
+	}
+
+	/**
+	 * @param string $tpl_var
+	 * @param mixed $value
+	 * @param bool $nocache
+	 *
+	 * @return $this
+	 *
+	 * @noinspection MissingParameterTypeDeclarationInspection
+	 */
+	final public function assignML(string $tpl_var, $value = null, bool $nocache = false) : self
+	{
+		$this->cached_variables[$tpl_var] = ['value' => $value, 'nocache' => $nocache];
+		return $this;
+	}
+
+	/**
+	 * Returns multilingual cache or compile id, which includes the short language name as prefix.
+	 *
+	 * @param null|mixed $id Original cache or compile id
+	 *
+	 * @return string Modified id
+	 *
+	 * @noinspection MissingParameterTypeDeclarationInspection
+	 */
+	private function mlCacheCompileId($id = null) : string
+	{
+		return $this->language . '-' . $id;
+	}
+
+	/** @noinspection MethodShouldBeFinalInspection */
+	public function display($template = null, $cache_id = null, $compile_id = null, $parent = null) : void
+	{
+		// First: Load language
+		$this->lang_load();
+
+		// Then: Parse ML-variables
+		foreach ($this->cached_variables as $variable_name => $variable_content) {
+			$value = $variable_content['value'];
+			if (is_string($value)) {
+				$value = $this->lang_decode($value, null);
+			}
+
+			$this->assign($variable_name, $value, $variable_content['nocache']);
+		}
+
+		// We need to set the cache id and compile id so a new script will be
+		// compiled for each language. This makes things really fast ;-)
+		$cache_id = $this->mlCacheCompileId($cache_id ?? $this->cache_id);
+		$compile_id = $this->mlCacheCompileId($compile_id ?? $this->compile_id);
+
+		// Finally: Parse and display template
+		parent::display($template, $cache_id, $compile_id, $parent);
+	}
+
+	/** @noinspection MethodShouldBeFinalInspection */
+	public function fetch($template = null, $cache_id = null, $compile_id = null, $parent = null) : string
+	{
+		// We need to set the cache id and compile id so a new script will be
+		// compiled for each language. This makes things really fast ;-)
+		$cache_id = $this->mlCacheCompileId($cache_id ?? $this->cache_id);
+		$compile_id = $this->mlCacheCompileId($compile_id ?? $this->compile_id);
+
+		// Call parent method with modified cache and compile ids
+		return parent::fetch($template, $cache_id, $compile_id, $parent);
+	}
+
+	/**
+	 * Returns current language.
+	 *
+	 * @return string Short name of language
+	 *
+	 * @noinspection MethodShouldBeFinalInspection
+	 */
+	public function getLanguage() : string
+	{
+		return $this->language;
+	}
+
+	/**
+	 * Return allowed languages.
+	 *
+	 * @return string[] List of language short names (like "en" for English, "es" for Spanish, ...)
+	 */
+	final public function getLanguages() : array
+	{
+		return $this->languages;
+	}
+
+	/**
+	 * Test to see if valid cache exists for this template
+	 *
+	 * @param null|Smarty_Internal_Template|string $template name of template file
+	 * @param null|string $cache_id
+	 * @param null|string $compile_id
+	 * @param null|object $parent
+	 *
+	 * @return bool results of {@link _read_cache_file()}
+	 * @throws SmartyException
+	 *
+	 * @noinspection MethodShouldBeFinalInspection
+	 */
+	public function isCached($template = null, $cache_id = null, $compile_id = null, $parent = null) : bool
+	{
+		if (!$this->caching) {
+			return false;
+		}
+
+		$cache_id = $this->mlCacheCompileId($cache_id ?? $this->cache_id);
+		$compile_id = $this->mlCacheCompileId($compile_id ?? $this->compile_id);
+
+		return parent::isCached($template, $cache_id, $compile_id, $parent);
+	}
+
+	/**
+	 * Checks a line of the language file, if it is eligible for language translation.
+	 * Empty, commented and lines without an equal sign are rejected.
+	 *
+	 * @param string $row
+	 *
+	 * @return bool
+	 */
+	private static function isLanguageLine(string $row) : bool
+	{
+		return mb_strlen($row) > 2 && strncmp($row, '//', 2) !== 0 && mb_strpos($row, '=') !== false;
+	}
+
+	/** @noinspection PhpUnusedParameterInspection */
+	final public function lang_decode(string $tpl_source, ?Smarty_Internal_Template $template) : string
+	{
+		return preg_replace_callback('/##(.+?)##/', function(array $text) : string {
+			return $this->translations[$text[1]] ?? '';
+		}, $tpl_source);
+	}
+
+	/**
+	 * Loads a language file and builds a translation table.
+	 *
+	 * @throws SmartyException Language file is not found or cannot be opened/read
+	 */
+	private function lang_load() : void
+	{
+		$lang_file = $this->locales_dir . $this->language . '.txt';
+		if (file_exists($lang_file)) {
+			$file = file($lang_file);
+			if ($file === false) {
+				throw new SmartyException('Could not read language file "' . $lang_file . '"');
+			}
+
+			$translations = [];
+			foreach ($file as $row) {
+				// ignore empty rows and comments
+				if (self::isLanguageLine($row)) {
+					[$match, $replacement] = explode('=', $row, 2);
+
+					// ignore last character, if it is a line break
+					if (substr($replacement, -1) === "\n") {
+						$replacement = substr($replacement, 0, -1);
+					}
+
+					$translations[$match] = $replacement;
+				}
+			}
+			$this->translations = $translations;
+		} else {
+			throw new SmartyException('Language file was not found at "' . $lang_file . '"');
+		}
+	}
+
+	/**
+	 * Changes the language or resets value to default.
+	 *
+	 * @param null|string $lang The language. If null, the default language will be used
+	 *
+	 * @return void
+	 * @throws SmartyException Invalid language
+	 */
+	final public function setLanguage(string $lang = null) : void
+	{
+		$lang = $lang ?? $this->default_language;
+
+		// Verifying language
+		if (!in_array($lang, $this->languages, true)) {
+			throw new SmartyException('Given language "' . $lang . '" is not allowed!');
+		}
+
+		$this->language = $lang;
+		$this->assign('lang', $lang);
+	}
+}
+
+
+?>

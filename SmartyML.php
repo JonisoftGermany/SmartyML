8c53d6b8b015e5ae609559d2ef0309f0e8658cc9
Stoll, Jonathan <git@jonathanstoll.de>
2023-01-08 08:39:06 +0100

    Fix Assignment of Lang-variable

--- SmartyML.php
+++ SmartyML.php
@@ -7,7 +7,7 @@ use SPFW\system\config\SmartyMLConfig;
  * Smarty Multilingual 2.0
  *
  * @author Jonathan Stoll
- * @version 12
+ * @version 13
  */
 class SmartyML extends Smarty
 {
@@ -101,6 +101,7 @@ class SmartyML extends Smarty
 	{
 		// First: Load language
 		$this->lang_load();
+		$this->assign('lang', $this->language);
 
 		// Then: Parse ML-variables
 		foreach ($this->cached_variables as $variable_name => $variable_content) {
@@ -277,7 +278,6 @@ class SmartyML extends Smarty
 		}
 
 		$this->language = $lang;
-		$this->assign('lang', $lang);
 	}
 }
 
